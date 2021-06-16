2cd9766b2ea08ef30fd01cd2fe1ded6a6749d78b
tomprater <tomoliverprater@gmail.com>
2021-06-16 13:45:08 +0100

    Single map and chart

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,65 +87,191 @@ for (const url of imageURLs) preloadImage(url);
 
 // MAPS
 
-let historicalSlider = document.getElementById("historicalRange");
-        let rcpSlider = document.getElementById("rcpRange");
-        let historicalYear = document.getElementById("historical-year");
+        let slider = document.getElementById("yearRange");
+        let year = document.getElementById("year");
         let rcpYear = document.getElementById("rcp-year");
         let scenario = document.getElementById("scenario");
-        let historicalGlobe = document.getElementById("historical-globe");
-        let rcpGlobe = document.getElementById("rcp-globe");
+        let scenarioYear = document.getElementById("scenario-year");
+        let pHValue = document.getElementById("value");
+        let globe = document.getElementById("globe");
         let chart = document.getElementById('globe-chart');
-        let chartrcp = document.getElementById('globe-chart-rcp');
-        historicalYear.innerHTML = historicalSlider.value;
-        rcpYear.innerHTML = rcpSlider.value; 
+        year.innerHTML = slider.value;
+
+        // BUTTONS
+
+        const pathColours = {
+          'historical': '#0b4572',
+          '2.6': '#20998a',
+          '4.5': '#efc530',
+          '6.0': '#c7432b',
+          '8.5': '#740707'
+        }
+
+        const historicalPhValues = {
+          1770: '8.18',
+          1780: '8.178',
+          1790: '8.176',
+          1800: '8.174',
+          1810: '8.173',
+          1820: '8.172',
+          1830: '8.172',
+          1840: '8.173',
+          1850: '8.172',
+          1860: '8.17',
+          1870: '8.168',
+          1880: '8.164',
+          1890: '8.159',
+          1900: '8.153',
+          1910: '8.151',
+          1920: '8.149',
+          1930: '8.142',
+          1940: '8.137',
+          1950: '8.128',
+          1960: '8.116',
+          1970: '8.106',
+          1980: '8.097',
+          1990: '8.085',
+          2000: '8.069'
+        }
+        const one = {
+          2010: '8.066',
+          2020: '8.053',
+          2030: '8.029',
+          2040: '8.014',
+          2050: '8.011',
+          2060: '8.009',
+          2070: '8.011',
+          2080: '8.016',
+          2090: '8.022',
+          2100: '8.027'
+        }
+
+        const two = {
+          2010: '8.066',
+          2020: '8.055',
+          2030: '8.028',
+          2040: '8.003',
+          2050: '7.979',
+          2060: '7.956',
+          2070: '7.939',
+          2080: '7.930',
+          2090: '7.927',
+          2100: '7.919'
+        }
+
+        const three = {
+          2010: '8.066',
+          2020: '8.055',
+          2030: '8.033',
+          2040: '8.010',
+          2050: '7.985',
+          2060: '7.959',
+          2070: '7.927',
+          2080: '7.893',
+          2090: '7.859',
+          2100: '7.836'
+        }
+
+        const four = {
+          2010: '8.066',
+          2020: '8.050',
+          2030: '8.017',
+          2040: '7.980',
+          2050: '7.940',
+          2060: '7.892',
+          2070: '7.841',
+          2080: '7.789',
+          2090: '7.741',
+          2100: '7.693'
+        }
+
+        
 
         $(".buttons button").click(function(){
         $(".buttons button").removeClass("active");
         $(this).addClass("active");
         var currentClass = $(".buttons button.active").val();
         console.log(currentClass);
-        if (currentClass == 'historical'){
-          chart.src = "assets/img/maps/chart/"+currentClass+"/"+historicalYear.innerHTML+".png";
-          $(".chart-year").text(historicalYear.innerHTML);
-          $('#historical-container').addClass("active");
-          $('#rcp-container').removeClass("active");
-          $(this).removeClass("active");
+        if (currentClass == 'historical' && year.innerHTML >= 2010){
+          slider.value = 2000
+          $("#year").text(slider.value);
+        } else if (currentClass != 'historical' && year.innerHTML <= 2000){
+          slider.value = 2010
+          $("#year").text(slider.value);
+        }
+      
+        if (currentClass != 'historical' && slider.value >= 2010){
+          scenario.style.display = 'inline'
+          scenario.innerHTML = ' (RCP'+currentClass+')';
         }
         else{
-        $(".chart-year").text("RCP "+this.value+": "+rcpYear.innerHTML);
-        rcpGlobe.src = "assets/img/maps/"+currentClass+"/"+rcpYear.innerHTML+".png";
-        chart.src = "assets/img/maps/chart/"+currentClass+"/"+rcpYear.innerHTML+".png";
-        chartrcp.src = "assets/img/maps/chart-rcp/"+currentClass+"/"+rcpYear.innerHTML+".png";
-        scenario.innerHTML = 'RCP'+currentClass+': ';
-        $('#historical-container').removeClass("active");
-        $('#rcp-container').addClass("active");
-        //console.log(historicalSlider.value)
-        //console.log(rcpSlider.value)
+          scenario.style.display = 'none'
         }
+
+        globe.src = "assets/img/maps/"+currentClass+"/"+year.innerHTML+".png";
+        chart.src = "assets/img/maps/chart/"+currentClass+"/"+year.innerHTML+".png";
+        
+        $(scenarioYear).css('color', pathColours[currentClass])
+
+        if (currentClass == '2.6'){
+          $(pHValue).text(one[slider.value])
+          } else if (currentClass == '4.5'){
+            $(pHValue).text(two[slider.value])
+            } else if (currentClass == '6.0'){
+              $(pHValue).text(three[slider.value])
+              } else if (currentClass == '8.5'){
+                $(pHValue).text(four[slider.value])
+                } else {
+            $(pHValue).text(historicalPhValues[slider.value])
+          }
+
         });
 
-        // Update the current slider value (each time you drag the slider handle)
-        historicalSlider.oninput = function() {
-        var target = $(".buttons button.current").data("class");
-        historicalGlobe.src = "assets/img/maps/"+target+"/"+this.value+".png";
-        chart.src = "assets/img/maps/chart/"+target+"/"+this.value+".png";
-        historicalYear.innerHTML = this.value;
-        // $(".chart-year").text(this.value);
+        let target = $(".buttons button.active").data("class");
+        // SLIDER
+        slider.oninput = function() {
+        
+          let target = $(".buttons button.active").data("class");
+        if (this.value >= 2010 && target == 'historical'){
+          $(".buttons button").removeClass("active");
+          $(".buttons button#rcp2_6").addClass("active");
+          console.log('wrong')
+          target = '2.6';
         }
 
-        rcpSlider.oninput = function() {
-        var target = $(".buttons button.active").data("class");
-        rcpGlobe.src = "assets/img/maps/"+target+"/"+this.value+".png";
+        else if (this.value <= 2000 && target != 'historical'){
+          $(".buttons button").removeClass("active");
+          $(".buttons button#historical").addClass("active");
+          target = 'historical';
+          $(scenarioYear).css('color', '#0b4572')
+        } 
+        if (target != 'historical' && slider.value >= 2010){
+          scenario.style.display = 'inline';
+          scenario.innerHTML = ' (RCP'+target+')';
+        }
+        else{
+          scenario.style.display = 'none'
+        }
+
+        globe.src = "assets/img/maps/"+target+"/"+this.value+".png";
         chart.src = "assets/img/maps/chart/"+target+"/"+this.value+".png";
-        chartrcp.src = "assets/img/maps/chart-rcp/"+target+"/"+this.value+".png";
-        // scenario.innerHTML = 'RCP'+target+': ';
-        rcpYear.innerHTML = this.value;
-        //$("#scenario").text("RCP "+target+": "+rcpYear.innerHTML);
-        $(".chart-year").text("RCP"+target+": "+rcpYear.innerHTML);
+        year.innerHTML = this.value;
+        $(scenarioYear).css('color', pathColours[target])
+        if (target == '2.6'){
+        $(pHValue).text(one[slider.value])
+        } else if (target == '4.5'){
+          $(pHValue).text(two[slider.value])
+          } else if (target == '6.0'){
+            $(pHValue).text(three[slider.value])
+            } else if (target == '8.5'){
+              $(pHValue).text(four[slider.value])
+              } else {
+          $(pHValue).text(historicalPhValues[slider.value])
+        }
         }
 
 
-        // END MAPS
+// END MAPS
 
 // scroll to top on window reload
 // window.onbeforeunload = function () {
